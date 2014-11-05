7e3144e825caf5064d0c232829d5d75f26f64efe
xuhao <hao.xu@cs2c.com.cn>
2014-11-05 16:25:30 +0800

    modify strings.xml

--- src/com/snack/main/content/DetailContent.java
+++ src/com/snack/main/content/DetailContent.java
@@ -1,5 +1,30 @@
 package com.snack.main.content;
 
-public class DetailContent {
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * @author Administrator
+ *
+ */
+public class DetailContent {
+	public List<DetailItem> ITEMS = new ArrayList<DetailItem>();
+	
+	/**
+	 * An item representing a piece of content.
+	 */
+	public static class DetailItem {
+		public String id;
+		public String content;
+
+		public DetailItem(String id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+
+		@Override
+		public String toString() {
+			return content;
+		}
+	}
 }
