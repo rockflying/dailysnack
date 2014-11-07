ff293034d5681c2c46c6a3d7ac4049ad9b42b878
xuhao <rockflyme@gmail.com>
2014-11-07 17:12:23 +0800

    add ItemList

---
+++ src/com/snack/main/ItemListActivity.java
@@ -0,0 +1,7 @@
+package com.snack.main;
+
+import android.support.v4.app.FragmentActivity;
+
+public class ItemListActivity extends FragmentActivity {
+
+}
---
+++ src/com/snack/main/ItemListFragment.java
@@ -0,0 +1,7 @@
+package com.snack.main;
+
+import android.support.v4.app.ListFragment;
+
+public class ItemListFragment extends ListFragment{
+
+}
