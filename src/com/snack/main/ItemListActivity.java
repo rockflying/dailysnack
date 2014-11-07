2202ce12ad2f2e97b50fc36370e406bea8e70021
xuhao <rockflyme@gmail.com>
2014-11-07 17:33:02 +0800

    add layout xml

--- src/com/snack/main/ItemListActivity.java
+++ src/com/snack/main/ItemListActivity.java
@@ -1,7 +1,13 @@
 package com.snack.main;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
 public class ItemListActivity extends FragmentActivity {
-
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.id.sub_item_list);
+	}
 }
