0ad48f55e9af199d4f80c05d63bbc7bb019f41cc
xuhao <rockflyme@gmail.com>
2014-11-07 17:39:53 +0800

    udpate

--- src/com/snack/main/ItemListFragment.java
+++ src/com/snack/main/ItemListFragment.java
@@ -1,7 +1,15 @@
 package com.snack.main;
 
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 public class ItemListFragment extends ListFragment{
-
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		setListAdapter(new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_activated_1,
+				android.R.id.text1, new String[] {"123", "456", "789"}));
+	}
 }
